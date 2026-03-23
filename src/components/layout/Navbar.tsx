ca503d9c40acc1ad80dfa193ab5dc8d6beb6fa7e
Barnabas Affonshike <117881010+Barnabas001@users.noreply.github.com>
2026-03-23 15:12:23 +0100

    make cart icon link to cart

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -61,7 +61,7 @@ export default function Navbar() {
         <Link to="/cart" className="relative ml-auto md:ml-0">
           <ShoppingCart className="w-6 h-6" />
           {cartCount > 0 && (
-            <span className="absolute -top-2 -right-2 bg-blue-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center">
+            <span className="absolute -top-2 -right-2 bg-blue-500 text-white text-xs w-5 h-5 rounded-full flex items-center justify-center font-semibold">
               {cartCount}
             </span>
           )}
